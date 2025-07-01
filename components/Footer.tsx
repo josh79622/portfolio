1f1385ca72bc662488575bd84814b78c3f69688b
Cheng-En <cheng-en.tsai@student.uts.edu.au>
2025-07-01 18:25:40 +1000

    fix some eslint issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
           {
             socialMedia.map((profile) => (
               <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-gray-800 rounded-lg border border-gray-700">
-                <img src={profile.img} alt={profile.id} width={20} height={20} />
+                <img src={profile.img} alt={`${profile.id}`} width={20} height={20} />
               </div>
             ))
           }
